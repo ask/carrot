f497ed90b2e6cc618af5e70ab1d2dad11f44c4fe
Ask Solem <askh@modwheel.net>
2009-03-24 17:46:31 +0100

    Fixed typo package -> packages

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     description=carrot.__doc__,
     author=carrot.__author__,
     author_email=carrot.__contact__,
-    package=find_packages(exclude=['ez_setup']),
+    packages=find_packages(exclude=['ez_setup']),
     install_requires=[
         'django>=1.0',
         'amqplib',
