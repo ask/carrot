d066cf663ccc3fe35f6447d378312ec8a1a8085f
Ask Solem <askh@modwheel.net>
2009-03-25 16:11:22 +0100

    Bumped version to 0.2.2

--- carrot/__init__.py
+++ carrot/__init__.py
@@ -1,5 +1,5 @@
-"""Easy RabbitMQ messaging queue support for Django."""
-VERSION = (0, 2, 1)
+"""Easy RabbitMQ/ZeroMQ (AMQP) messaging queue."""
+VERSION = (0, 2, 2)
 __version__ = ".".join(map(str, VERSION))
 __author__ = "Ask Solem"
 __contact__ = "askh@opera.com"
