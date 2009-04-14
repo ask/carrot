751f6cbe4a2c6203331071568bc0a78264c60a10
Ask Solem <askh@modwheel.net>
2009-04-14 11:13:17 +0200

    Bumped version to 0.3.0 and set __docformat__ to restructured text

--- carrot/__init__.py
+++ carrot/__init__.py
@@ -1,5 +1,6 @@
 """Easy RabbitMQ/ZeroMQ (AMQP) messaging queue."""
-VERSION = (0, 2, 2)
+VERSION = (0, 3, 0)
 __version__ = ".".join(map(str, VERSION))
 __author__ = "Ask Solem"
 __contact__ = "askh@opera.com"
+__docformat__ = "restructuredtext"
