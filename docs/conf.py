95abea92b4f4465f35f08566242c41aa0c8d4cd5
Ask Solem <askh@opera.com>
2009-05-27 21:02:51 +0200

    "../" not "../carrot"

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ import os
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
-sys.path.insert(0, "../carrot")
+sys.path.insert(0, "../")
 import carrot
 
 # General configuration
