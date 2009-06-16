1a27cfc3060bfa3197c16ccf3b19749ef36dee9e
Ask Solem <askh@opera.com>
2009-06-16 22:26:06 +0200

    Updated Changelog and Bumped version to 0.4.5

--- carrot/__init__.py
+++ carrot/__init__.py
@@ -1,5 +1,5 @@
 """AMQP Messaging Framework for Python"""
-VERSION = (0, 4, 4)
+VERSION = (0, 4, 5)
 __version__ = ".".join(map(str, VERSION))
 __author__ = "Ask Solem"
 __contact__ = "askh@opera.com"
