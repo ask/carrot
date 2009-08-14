cb37436dc3ae7b8380b260cca9109d11f9a7a004
Ask Solem <askh@opera.com>
2009-08-14 13:09:12 +0200

    Fix backend aliases (AMQP -> amqplib, STOMP -> pystomp)

--- carrot/backends/__init__.py
+++ carrot/backends/__init__.py
@@ -7,10 +7,12 @@ import sys
 from functools import partial
 
 BACKEND_ALIASES = {
+    "amqp": "pyamqplib",
     "amqplib": "pyamqplib",
     "stomp": "pystomp",
     "stompy": "pystomp",
     "memory": "queue",
+    "mem": "queue",
 }
 
 
@@ -40,7 +42,7 @@ def get_backend_cls(backend):
 
     """
     if backend.find(".") == -1:
-        alias_to = BACKEND_ALIASES.get("backend", None)
+        alias_to = BACKEND_ALIASES.get(backend.lower(), None)
         backend = "carrot.backends.%s" % (alias_to or backend)
 
     __import__(backend)
