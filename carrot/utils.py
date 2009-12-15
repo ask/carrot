d9e048ef29489741a44fc709c87c6b802ced76a6
Ask Solem <askh@opera.com>
2009-12-15 13:56:33 +0100

    Compat partition+rpartition, and don't redefine str.

--- carrot/utils.py
+++ carrot/utils.py
@@ -19,20 +19,38 @@ def gen_unique_id():
     return str(uuid4())
 
 
-def _compat_rpartition(str, sep):
-    """``partition(str, sep) -> (head, sep, tail)``
-
-    Search for the separator ``sep`` in ``str``, and return the part before
-    it, the separator itself, and the part after it.  If the separator is not
-    found, return S and two empty strings.
-
-    """
-    items = str.rsplit(sep, 1)
+def _compat_rl_partition(S, sep, direction=str.split):
+    items = direction(S, sep, 1)
     if len(items) == 1:
         return items[0], sep, ''
     return items[0], sep, items[1]
 
+
+def _compat_partition(S, sep):
+    """``partition(S, sep) -> (head, sep, tail)``
+
+    Search for the separator ``sep`` in ``S``, and return the part before
+    it, the separator itself, and the part after it. If the separator is not
+    found, return ``S`` and two empty strings.
+
+    """
+    return _compat_rl_partition(S, sep, direction=str.split)
+
+
+def _compat_rpartition(S, sep):
+    """``rpartition(S, sep) -> (tail, sep, head)``
+
+    Search for the separator ``sep`` in ``S``, starting at the end of ``S``,
+    and return the part before it, the separator itself, and the part
+    after it. If the separator is not found, return two empty
+    strings and ``S``.
+
+    """
+    return _compat_rl_partition(S, sep, direction=str.rsplit)
+
 try:
+    partition = str.partition
     rpartition = str.rpartition
 except AttributeError: # Python <= 2.4
+    partition = _compat_partition
     rpartition = _compat_rpartition
