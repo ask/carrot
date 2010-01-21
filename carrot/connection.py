522b200bb8d4df77e6115e6cd9895f46bf99e8df
Ask Solem <askh@opera.com>
2010-01-21 23:55:30 +0100

    Probably not so useful, over-engineered connection pool. Have to make it into something useful.

--- carrot/connection.py
+++ carrot/connection.py
@@ -3,10 +3,14 @@
 Getting a connection to the AMQP server.
 
 """
-from amqplib.client_0_8.connection import AMQPConnectionException
-from carrot.backends import get_backend_cls
-import warnings
 import socket
+import warnings
+import threading
+from Queue import Queue
+
+from amqplib.client_0_8.connection import AMQPConnectionException
+
+from carrot.backends import get_backend_cls
 
 DEFAULT_CONNECT_TIMEOUT = 5 # seconds
 SETTING_PREFIX = "BROKER"
@@ -85,20 +89,16 @@ class BrokerConnection(object):
     virtual_host = "/"
     port = None
     insist = False
-    connect_timeout = DEFAULT_CONNECT_TIMEOUT
     ssl = False
-    _closed = True
     backend_cls = None
+    _closed = False
+    _released = False
+    connect_timeout = DEFAULT_CONNECT_TIMEOUT
 
     ConnectionException = AMQPConnectionException
 
-    @property
-    def host(self):
-        """The host as a hostname/port pair separated by colon."""
-        return ":".join([self.hostname, str(self.port)])
-
     def __init__(self, hostname=None, userid=None, password=None,
-            virtual_host=None, port=None, **kwargs):
+            virtual_host=None, port=None, pool=None, **kwargs):
         self.hostname = hostname
         self.userid = userid
         self.password = password
@@ -109,35 +109,38 @@ class BrokerConnection(object):
                                           self.connect_timeout)
         self.ssl = kwargs.get("ssl", self.ssl)
         self.backend_cls = kwargs.get("backend_cls", None)
-        self._closed = None
+        self._pool = pool
+        self._closed = False
         self._connection = None
 
-    @property
-    def connection(self):
-        if self._closed == True:
+    def connect(self):
+        """Establish a connection to the AMQP server."""
+        self._closed = False
+        self.evaluate_connection()
+
+    def evaluate_connection(self):
+        if self._closed:
             return
+        if self._released:
+            raise ReferenceError("Connection already released")
         if not self._connection:
             self._connection = self._establish_connection()
             self._closed = False
         return self._connection
 
-    def __enter__(self):
-        return self
+    def reconnect(self):
+        """Immediately close the current connection and establish a new."""
+        self.force_close_connection()
+        self.connect()
 
-    def __exit__(self, e_type, e_value, e_trace):
-        if e_type:
-            raise e_type(e_value)
-        self.close()
-
-    def _establish_connection(self):
-        return self.create_backend().establish_connection()
-
-    def get_backend_cls(self):
-        """Get the currently used backend class."""
-        backend_cls = self.backend_cls
-        if not backend_cls or isinstance(backend_cls, basestring):
-            backend_cls = get_backend_cls(backend_cls)
-        return backend_cls
+    def close(self):
+        """Close/release the connection."""
+        if self._pool:
+            if self._released:
+                raise ReferenceError("Connection already released.")
+            self._pool.release(self)
+        else:
+            self.force_close_connection()
 
     def create_backend(self):
         """Create a new instance of the current backend in
@@ -149,26 +152,155 @@ class BrokerConnection(object):
         """Request a new AMQP channel."""
         return self.connection.channel()
 
-    def connect(self):
-        """Establish a connection to the AMQP server."""
-        self._closed = False
-        return self.connection
+    def get_backend_cls(self):
+        """Get the currently used backend class."""
+        backend_cls = self.backend_cls
+        if not backend_cls or isinstance(backend_cls, basestring):
+            backend_cls = get_backend_cls(backend_cls)
+        return backend_cls
 
-    def close(self):
-        """Close the currently open connection."""
+    def force_close_connection(self):
+        """Force close the connection.
+
+        If you're not sure, please use :meth:`close` instead.
+
+        """
         try:
             if self._connection:
                 backend = self.create_backend()
                 backend.close_connection(self._connection)
         except socket.error:
             pass
-        self._closed = True
+        finally:
+            self._connection = None
 
-# For backwards compatability.
+    def _establish_connection(self):
+        return self.create_backend().establish_connection()
+
+    def __repr__(self):
+        infodict = self.info
+
+        def by_stringtype(k):
+            return isinstance(infodict[k], basestring) and "a" or "z"
+
+        prettyinfo = ", ".join("%s=%s" % (key, repr(infodict[key]))
+                                    for key in sorted(infodict.keys(),
+                                                      key=by_stringtype))
+        connected = self._connection is not None
+        state = connected and "connected" or "not connected"
+        return "<%s: %s (info->%s)>" % (self.__class__.__name__,
+                                        state,
+                                        prettyinfo)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, e_type, e_value, e_trace):
+        if e_type:
+            raise e_type(e_value)
+        self.close()
+
+    @property
+    def connection(self):
+        """Internal connection object."""
+        return self.evaluate_connection()
+
+    @property
+    def host(self):
+        """The host as a hostname/port pair separated by colon."""
+        return ":".join([self.hostname, str(self.port)])
+
+    @property
+    def info(self):
+        return {"userid": self.userid,
+                "password": self.password,
+                "hostname": self.hostname,
+                "backend_cls": self.backend_cls,
+                "virtual_host": self.virtual_host,
+                "connect_timeout": self.connect_timeout,
+                "insist": self.insist,
+                "port": self.port,
+                "ssl": self.ssl}
+
+# For backwards compatibility.
 AMQPConnection = BrokerConnection
 
 
-def get_django_conninfo():
+class ConnectionPool(object):
+
+    def __init__(self, connection_cls, size=4, maxsize=None, preconnect=0,
+            lost_errors=(IOError, socket.error), buffersize=2, initial=[], **kwargs):
+        self.connection_cls = connection_cls
+        self.kwargs = kwargs
+        self.maxsize = maxsize
+        self.size = size
+        self.cursize = 0
+        self.preconnect = 0
+        self.buffersize = buffersize
+        self.lost_errors = lost_errors
+        self.initial = initial
+
+        self._ready = Queue()
+        self._dirty = {}
+        self._add_lock = threading.RLock()
+
+        self._prepopulate()
+
+
+    def _prepopulate(self):
+        for i in range(max(self.preconnect, self.buffersize) - len(self.initial)):
+            self.add_connection(eager=i < self.preconnect)
+
+    @classmethod
+    def from_connection(cls, connection, **kwargs):
+        return cls(connection.__class__, **dict(connection.info, **kwargs))
+
+    def create_connection(self):
+        return self.connection_cls(**dict(self.kwargs, pool=self))
+
+    def add_connection(self, eager=False):
+        connection = self.create_connection()
+        self._ready.put_nowait(self.create_connection())
+        if eager:
+            connection.evaluate_connection()
+        self.cursize += 1
+
+    def is_alive(self, connection):
+        try:
+            return True # TODO
+        except self.lost_errors, exc:
+            return False
+
+    def _maintain_buffer(self):
+        if self.maxsize and len(self._dirty) >= self.maxsize:
+            return
+        missing = max(self.buffersize - len(self._ready.queue), 0)
+        if missing:
+            for i in range(missing + 1):
+                self.add_connection()
+
+    def _next(self, block, timeout):
+        while 1:
+            self._maintain_buffer()
+            connection = self._ready.get(block=block, timeout=timeout)
+            if self.is_alive(connection):
+                return connection
+
+    def close(self):
+        for connection in frozenset(self._ready.queue + self._dirty.keys()):
+            connection.force_close_connection()
+
+    def acquire(self, block=True, timeout=None):
+        return self._next(block, timeout)
+
+    def release(self, connection):
+        assert connection not in self._ready.queue
+        self._dirty.pop(connection, None)
+        if len(self._ready.queue) < self.size:
+            self._ready.put_nowait(connection)
+
+
+def _get_django_conninfo():
     # FIXME can't wait to remove this mess in 1.0 [askh]
     ci = {}
     from django.conf import settings as django_settings
@@ -222,7 +354,7 @@ class DjangoBrokerConnection(BrokerConnection):
 
 
     def __init__(self, *args, **kwargs):
-        kwargs = dict(get_django_conninfo(), **kwargs)
+        kwargs = dict(_get_django_conninfo(), **kwargs)
         super(DjangoBrokerConnection, self).__init__(*args, **kwargs)
 
 # For backwards compatability.
